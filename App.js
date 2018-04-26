9d01897f4f652fb466860a30579e28459246fd4a
matthi <mruhland01@gmail.com>
2018-04-26 12:13:20 -0700

    added functionality to add and remove list items

--- App.js
+++ App.js
@@ -1,48 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import ListItem from './src/components/ListItem/ListItem';
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 export default class App extends React.Component {
   state = {
-    placeName: "",
     places: []
   };
 
-  placeNameChangedHandler = (val) => {
-    this.setState({placeName: val});
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName === "") {
-      return;
-    }
+  onPlaceAdded = (placeName) => {
     this.setState(prevState => {
-      return {
-        places: prevState.places.concat(prevState.placeName)
-      };
+        return {
+        places: prevState.places.concat(placeName)
+        };
     });
   };
 
+  placeDeleted = (index) => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place, i) => i !== index)
+      };
+    });
+  }
+
   render() {
-    const placesOutput = this.state.places.map((place, i) => (
-      <ListItem key={i} placeName={place} />
-    ));
 
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput 
-            style={styles.placeInput}
-            placeholder= "An awesome place"
-            value={this.state.placeName} 
-            onChangeText={this.placeNameChangedHandler} 
-          />
-          <Button style={styles.placeButton} title="Add" onPress={this.placeSubmitHandler} />
-        </View>
-        <View style={styles.listContainer}>
-          {placesOutput}
-        </View>
+        <PlaceInput 
+          onPlaceAdded={this.onPlaceAdded} />
+        <PlaceList 
+          places={this.state.places} onItemDeleted={this.placeDeleted} />
       </View>
     );
   };
@@ -55,26 +45,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
-  },
-
-  inputContainer: {
-    //flex: 1,
-    width: "100%",
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-
-  placeInput: {
-    width: "70%"
-  },
-
-  placeButton: {
-    width: "30%"
-  },
-
-  listContainer: {
-    width: "100%"
   }
 
 });
---
+++ src/components/PlaceList/PlaceList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
+
+import ListItem from '../ListItem/ListItem';
+
+const PlaceList = (props) => {
+    const placesOutput = props.places.map((place, i) => (
+        <ListItem key={i} placeName={place} onItemPressed={() => props.onItemDeleted(i) } />
+    ));
+    
+    return (
+        <View style={styles.listContainer}>
+            {placesOutput}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    listContainer: {
+        width: "100%"
+    }
+});
+
+export default PlaceList;
