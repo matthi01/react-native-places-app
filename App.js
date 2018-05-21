56d70effbd3e1c18b12a55c21b68c4f87b804794
matthi <mruhland01@gmail.com>
2018-05-21 09:42:04 -0700

    setting up read-native-navigation

--- App.js
+++ App.js
@@ -1,71 +1,14 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import {connect} from 'react-redux';
+import { Navigation } from "react-native-navigation";
 
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-import * as actionCreators from './src/store/actions/index';
+import AuthScreen from "./src/screens/Auth/Auth";
 
-class App extends React.Component {
+//Register Screens
+Navigation.registerComponent("awesomeplaces.AuthScreen", () => AuthScreen);
 
-  onPlaceAdded = (placeName) => {
-    this.props.onAddPlace(placeName);
+//Start App (navigation)
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awesomeplaces.AuthScreen",
+    title: "Login"
   }
-
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  }
-
-  placeSelectedHandler = (key) => {
-    this.props.onSelectPlace(key);
-  }
-
-  modalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  }
-
-  render() {
-
-    return (
-      <View style={styles.container}>
-        <PlaceDetail 
-          selectedPlace={this.props.selectedPlace} onItemDeleted={this.placeDeletedHandler} onModalClosed={this.modalClosedHandler} />
-        <PlaceInput 
-          onPlaceAdded={this.onPlaceAdded} />
-        <PlaceList 
-          places={this.props.places} 
-          onItemSelected={this.placeSelectedHandler} />
-      </View>
-    );
-  };
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  }
-
 });
-
-const mapStateToProps = (state) => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapActionsToProps = (dispatch) => {
-  return {
-    onAddPlace: (name) => dispatch(actionCreators.addPlace(name)),
-    onDeletePlace: () => dispatch(actionCreators.deletePlace()),
-    onSelectPlace: (key) => dispatch(actionCreators.selectPlace(key)),
-    onDeselectPlace: () => dispatch(actionCreators.deselectPlace())
-  };
-};
-
-export default connect(mapStateToProps, mapActionsToProps)(App);
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import {Provider} from 'react-redux';
-import App from './App';
-import configureStore from './src/store/configureStore';
+import React from "react";
+import { AppRegistry } from "react-native";
+import { Provider } from "react-redux";
+import App from "./App";
+import configureStore from "./src/store/configureStore";
 
 const store = configureStore();
 
 // registerComponent expects a component.. or a function returning jsx
 const reactNativeRedux = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
-AppRegistry.registerComponent('awesomeplaces', () => reactNativeRedux);
+AppRegistry.registerComponent("awesomeplaces", () => reactNativeRedux);
