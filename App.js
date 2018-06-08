5fefa0daf2e7e291c1ca7a10a30d64f47d1a13fc
matthi <mruhland01@gmail.com>
2018-06-07 19:54:57 -0700

    small updates

--- App.js
+++ App.js
@@ -4,34 +4,39 @@ import { Provider } from "react-redux";
 import AuthScreen from "./src/screens/Auth/Auth";
 import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
 import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
+import PlaceDetailScreen from "./src/screens/PlaceDetai/PlaceDetai";
 import configureStore from "./src/store/configureStore";
 
 const store = configureStore();
 
 //Register Screens
 Navigation.registerComponent(
-  "awesomeplaces.AuthScreen",
-  () => AuthScreen,
-  store,
-  Provider
+    "awesomeplaces.AuthScreen",
+    () => AuthScreen,
+    store,
+    Provider
 );
 Navigation.registerComponent(
-  "awesomeplaces.SharePlaceScreen",
-  () => SharePlaceScreen,
-  store,
-  Provider
+    "awesomeplaces.SharePlaceScreen",
+    () => SharePlaceScreen,
+    store,
+    Provider
 );
 Navigation.registerComponent(
-  "awesomeplaces.FindPlaceScreen",
-  () => FindPlaceScreen,
-  store,
-  Provider
+    "awesomeplaces.FindPlaceScreen",
+    () => FindPlaceScreen,
+    store,
+    Provider
+);
+Navigation.registerComponent(
+    "awesomeplaces.PlaceDetailScreen",
+    () => PlaceDetailScreen
 );
 
 //Start App (navigation)
 Navigation.startSingleScreenApp({
-  screen: {
-    screen: "awesomeplaces.AuthScreen",
-    title: "Login"
-  }
+    screen: {
+        screen: "awesomeplaces.AuthScreen",
+        title: "Login"
+    }
 });
