fb26e145c8cdb3ab1299c3a90eda883e0d3ee393
matthi <mruhland01@gmail.com>
2018-09-27 09:09:47 -0700

    added reusable input components / starting work on login screen

---
+++ src/components/UI/DefaultInput/DefaultInput.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
+
+const defaultInput = props => (
+    <TextInput
+        underlineColorAndroid="transparent"
+        {...props}
+        // in case overriding styles are added into props make sure they are added to the stylesheet
+        style={[styles.input, props.style]}
+    />
+);
+
+const styles = StyleSheet.create({
+    input: {
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#eee",
+        padding: 5,
+        margin: 5
+    }
+});
+
+export default defaultInput;
