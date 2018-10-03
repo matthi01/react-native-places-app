36a77c4b83565b8aed5c86545c32d834ea36ee73
matthi <mruhland01@gmail.com>
2018-10-02 20:39:20 -0700

    added animation to location list

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -13,7 +13,8 @@ import PlaceList from "../../components/PlaceList/PlaceList";
 class FindPlaceScreen extends Component {
     state = {
         placesLoaded: false,
-        removeAnimation: new Animated.Value(1)
+        removeAnimation: new Animated.Value(1),
+        loadedAnimation: new Animated.Value(0)
     };
 
     constructor(props) {
@@ -47,12 +48,22 @@ class FindPlaceScreen extends Component {
         });
     };
 
+    placesLoadedHandler = () => {
+        Animated.timing(this.state.loadedAnimation, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true
+        }).start();
+    };
+
     placesSearchHandler = () => {
         Animated.timing(this.state.removeAnimation, {
             toValue: 0,
             duration: 500,
             useNativeDriver: true
-        }).start();
+        }).start(() => {
+            this.setState({ placesLoaded: true });
+        }, this.placesLoadedHandler());
     };
 
     render() {
@@ -62,7 +73,10 @@ class FindPlaceScreen extends Component {
                     opacity: this.state.removeAnimation,
                     transform: [
                         {
-                            scale: this.state.removeAnimation
+                            scale: this.state.removeAnimation.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [12, 1]
+                            })
                         }
                     ]
                 }}
@@ -77,10 +91,16 @@ class FindPlaceScreen extends Component {
 
         if (this.state.placesLoaded) {
             content = (
-                <PlaceList
-                    places={this.props.places}
-                    onItemSelected={this.itemSelectedHandler}
-                />
+                <Animated.View
+                    style={{
+                        opacity: this.state.loadedAnimation
+                    }}
+                >
+                    <PlaceList
+                        places={this.props.places}
+                        onItemSelected={this.itemSelectedHandler}
+                    />
+                </Animated.View>
             );
         }
         return (
